395312f5a9a16b33779248e2834c2b0358b63f3c
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-04-25 21:24:28 -0400

    added basic healthcheck to check that docker works

---
+++ app.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+from flask import Flask, jsonify, make_response, Response, request
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+# from flask_cors import CORS
+
+from config import ProductionConfig
+
+
+load_dotenv()
+
+def create_app(config_class=ProductionConfig):
+    app = Flask(__name__)
+
+    ####################################################
+    #
+    # Healthchecks
+    #
+    ####################################################
+
+    @app.route('/api/health', methods=['GET'])
+    def healthcheck() -> Response:
+        """
+        Health check route to verify the service is running.
+
+        Returns:
+            JSON response indicating the health status of the service.
+
+        """
+        app.logger.info("Health check endpoint hit")
+        return make_response(jsonify({
+            'status': 'success',
+            'message': 'Service is running'
+        }), 200)
+    
+    return app
+    
+if __name__ == '__main__':
+    app = create_app()
+    app.logger.info("Starting Flask app...")
+    try:
+        app.run(debug=True, host='0.0.0.0', port=5002)
+    except Exception as e:
+        app.logger.error(f"Flask app encountered an error: {e}")
+    finally:
+        app.logger.info("Flask app has stopped.")
